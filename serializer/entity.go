011e3d277573a7c05b0f53fa0a6f0d969e8ae0f7
皮卡秋landy <1747024205@qq.com>
2022-05-23 06:22:06 +0800

    m

--- serializer/entity.go
+++ serializer/entity.go
@@ -3,7 +3,7 @@ package serializer
 type Video struct {
 	Id            int64  `json:"id,omitempty"`
 	Author        User   `json:"author"`
-	PlayUrl       string `json:"play_url" json:"play_url,omitempty"`
+	PlayUrl       string `json:"play_url,omitempty"`
 	CoverUrl      string `json:"cover_url,omitempty"`
 	FavoriteCount int64  `json:"favorite_count,omitempty"`
 	CommentCount  int64  `json:"comment_count,omitempty"`
